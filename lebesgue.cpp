fc9ba70056ca932929a1af937f9d5ce9ec3fb83c
Karen Julieth Pérez Bohórquez <kjperezb@unal.edu.co>
2018-03-06 23:05:50 -0500

    Lebesgue con desigualdad

--- lebesgue.cpp
+++ lebesgue.cpp
@@ -5,12 +5,18 @@ double sumtan(double n);
  double pi=std::atan2(0,-1);
 int main(){
   double sum,term;
+  int a=0.0;
+
+  std::cout.precision(8);
    for(double i=1.0;i<=100.0;i++){
      sum=sumtan(i);
      term=1/(2*i+1)+2/pi*sum;
-
+    
      std::cout<<"p"<<i<<" "<<term<<std::endl;
+     if(0<=((4/(pi*pi))*std::log(2*i+1)+1-term) && ((4/(pi*pi))*std::log(2*i+1)+1-term)<=0.0106) a++; 
    }
+
+   std::cout<<a<<std::endl;
   return 0;
 
 }
